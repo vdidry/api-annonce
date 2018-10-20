56a88056bc18a71c6d2ffe0d671db44adf326fff
Vince <vdidry@gmail.com>
2018-10-20 15:25:30 +0200

    Ajout controlleur annonce avec formulaire de validation et service annonce

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,7 +2,39 @@
 
 namespace App\Controller;
 
-class AnnonceController
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
+use App\Service\AnnonceService;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class AnnonceController extends FOSRestController
 {
 
+    /**
+     * Créé une annonce
+     * @Rest\Post("/annonces")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\Service\AnnonceService               $annonceService
+     *
+     * @return \FOS\RestBundle\View\View
+     */
+    public function postArticle(Request $request, AnnonceService $annonceService): View
+    {
+        $annonce = new Annonce();
+
+        $annonceType = $this->createForm(AnnonceType::class, $annonce);
+
+        $annonceType->submit($request->request->all());
+        if ($annonceType->isValid()) {
+            $annonce = $annonceService->addAnnonce($annonce);
+            return View::create($annonce, Response::HTTP_CREATED);
+        } else {
+            return View::create($annonce, Response::HTTP_CREATED);
+        }
+    }
 }
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -1,8 +1,69 @@
 <?php
 
-namespace App\Entity;
+namespace App\Form;
 
-class AnnonceType
+use App\Entity\Annonce;
+use App\Entity\Categorie;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'titre',
+                TextType::class,
+                [
+                    'required' => true,
+                ]
+            )
+            ->add(
+                'contenu',
+                TextareaType::class,
+                [
+                    'required' => true,
+                ]
+            )
+            ->add(
+                'prix',
+                NumberType::class,
+                [
+                    'required' => true,
+                ]
+            )
+            ->add(
+                'categorie',
+                EntityType::class,
+                [
+                    'class'    => Categorie::class,
+                    'choice_label' => 'id',
+                    'required' => true
+                ]
+            )
+            ->add(
+                'utilisateur',
+                EntityType::class,
+                [
+                    'class'    => Utilisateur::class,
+                    'choice_label' => 'id',
+                    'required' => true,
+                ]
+            );
+    }
 
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+            'csrf_protection' => false
+        ]);
+    }
 }
